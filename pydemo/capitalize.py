5f664d83a8dc587fb5a3753e4032f2832af90ec2
Jianwei Han <hanjianwei@gmail.com>
2023-04-20 21:59:51 +0800

    Add lowercase

--- pydemo/capitalize.py
+++ pydemo/capitalize.py
@@ -2,3 +2,6 @@ def capital_case(x):
     if not isinstance(x, str):
         raise TypeError('Please provide a string argument')
     return x.capitalize()
+
+def lower_case(x):
+    return x
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,4 +1,7 @@
-from pydemo.capitalize import capital_case
+from pydemo.capitalize import capital_case, lower_case
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
+
+def test_lower_case():
+    assert lower_case('SEMAPHORE') == 'semaphore'
